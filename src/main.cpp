0654475550c61c200ef6a13599475c2f6b0904eb
Jan Losinski <losinski@wh2.tu-dresden.de>
2018-01-08 10:43:14 +0100

    Make the hardware blink.
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,12 @@ disco
 10 led
 */
 
-#define DATA_PIN 13
+#define DATA_PIN_TOP 13
+#define DATA_PIN_BOTTOM 12
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
-#define NUM_LEDS 10
+#define NUM_LEDS 12
+#define LED_ROW 6
 
 #define BRIGHTNESS 96
 
@@ -27,24 +29,28 @@ uint8_t pins[] = {2, 3, 4, 5};
 CRGB colors[] = {CRGB::Red, CRGB::White, CRGB::Yellow, CRGB::LightSkyBlue};
 
 CRGB currentColor = CRGB::White;
-CRGB leds[NUM_LEDS];
+CRGBArray<NUM_LEDS> all_leds; 
 
 void setup() {
   for (uint8_t i = 0; i < 4; ++i) {
     pinMode(pins[i], INPUT);
   }
 
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+  FastLED.addLeds<LED_TYPE, DATA_PIN_TOP, COLOR_ORDER>(all_leds,LED_ROW)
+      .setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, DATA_PIN_BOTTOM, COLOR_ORDER>(all_leds+LED_ROW,LED_ROW)
       .setCorrection(TypicalLEDStrip);
 }
 
 void fill(CRGB color) {
-  for (uint8_t i = 0; i < NUM_LEDS; ++i) {
-    leds[i] = color;
-  }
+    all_leds = color;
 }
 
+
+uint8_t i = 0;
 void loop() {
+
+  all_leds.fill_rainbow(i++);
   for (uint8_t i = 0; i < 4; ++i) {
     int isPressed = digitalRead(pins[i]);
     if (isPressed) {
@@ -53,9 +59,9 @@ void loop() {
     }
   }
 
-  uint16_t val = analogRead(14);
+//  uint16_t val = analogRead(14);
 
-  FastLED.setBrightness(val / 4);
+//  FastLED.setBrightness(val / 4);
 
   FastLED.show();
 }
